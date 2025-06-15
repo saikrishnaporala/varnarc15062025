8571d854b216a551d8b288a7263b813cd996ffc4
saiporala <saikrishnaporala@gmail.com>
2025-06-15 21:16:38 +0530

    env

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,7 +66,5 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-
-        'admin.token' => \App\Http\Middleware\AdminTokenMiddleware::class,
     ];
 }
