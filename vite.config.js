78831c518fda8f98c50e0784f20a70ee1da94310
saiporala <saikrishnaporala@gmail.com>
2025-06-20 23:07:56 +0530

    update it with specific name

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     build: {
         outDir: 'public/build',
-        manifest: 'public/build/manifest.json',
+        manifest: 'manifest.json',
         emptyOutDir: true,
         chunkSizeWarningLimit: 4000
     },
